534ce66a725a2dd83b6df7fe05b59a9ef2246cca
JYLinOK <jylin1@163.com>
2024-07-29 22:13:04 +0800

    DSW008-SpringBoot-MongoDB-REST

--- code/S6-dsw-mongodb-rest/S6-dsw-mongodb-rest/src/main/java/com/jinwei/S6_dsw_mongodb_rest/Connector.java
+++ code/S6-dsw-mongodb-rest/S6-dsw-mongodb-rest/src/main/java/com/jinwei/S6_dsw_mongodb_rest/Connector.java
@@ -1,2 +1,52 @@
-package com.jinwei.S6_dsw_mongodb_rest;public class Connector {
+package com.jinwei.S6_dsw_mongodb_rest;
+import org.springframework.data.annotation.Id;
+
+
+public class Connector {
+
+    @Id private String id;
+
+    // 定义实例类需要 set-get 的各种属性
+    private String cacert;
+    private String description;
+    private String sellDataID;
+    private String buyDataID;
+
+    // get and set: cacert
+    public String getCacert() {
+        return cacert;
+    }
+
+    public void setCacert(String cacert) {
+        this.cacert = cacert;
+    }
+
+    // get and set: description
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // get and set: sellDataID
+    public String getSellDataID() {
+        return sellDataID;
+    }
+
+    public void setSellDataID(String sellDataID) {
+        this.sellDataID = sellDataID;
+    }
+
+    // get and set: buyDataID
+    public String getDuyDataID() {
+        return buyDataID;
+    }
+
+    public void setDuyDataID(String buyDataID) {
+        this.buyDataID = buyDataID;
+    }
 }
+
+
--- code/S6-dsw-mongodb-rest/S6-dsw-mongodb-rest/src/main/java/com/jinwei/S6_dsw_mongodb_rest/ConnectorRepository.java
+++ code/S6-dsw-mongodb-rest/S6-dsw-mongodb-rest/src/main/java/com/jinwei/S6_dsw_mongodb_rest/ConnectorRepository.java
@@ -1,2 +1,13 @@
-package com.jinwei.S6_dsw_mongodb_rest;public class ConnectorRepository {
+package com.jinwei.S6_dsw_mongodb_rest;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface ConnectorRepository extends MongoRepository<Connector, String> {
+
+    List<Connector> findByCacert(@Param("caert") String caert);
+
 }
